464cf7213c22cd2d076bd2b1024d17a780816632
saddam-khan-dev <f.saddam319@gmail.com>
2024-09-11 22:12:32 +0500

    container start

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-header': 'linear-gradient(95.17deg, rgba(32, 245, 115, 0.16) 14.04%, rgba(32, 245, 115, 0.07) 73.87%)',
+      },
+    },
   },
   plugins: [],
 }
-

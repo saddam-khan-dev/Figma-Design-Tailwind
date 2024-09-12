20e3c1f8fcef3c1119ff3cd6d0fd84cfd3ba6fa5
saddam-khan-dev <f.saddam319@gmail.com>
2024-09-12 17:20:55 +0500

    add custom colors in config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     extend: {
       backgroundImage: {
         'custom-header': 'linear-gradient(95.17deg, rgba(32, 245, 115, 0.16) 14.04%, rgba(32, 245, 115, 0.07) 73.87%)',
+        //Define custom gradient
+      },
+      colors: {
+        'custom-green': 'rgba(32, 245, 115, 1)',  // Define the custom color
       },
     },
   },

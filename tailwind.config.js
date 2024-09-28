214560744b96cc9a7ab277ebaa1a06580445b481
saddam-khan-dev <f.saddam319@gmail.com>
2024-09-28 12:58:56 +0500

    timeline working

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,31 @@
 module.exports = {
   content: ["*"],
   theme: {
+    screens: {
+      'xs': '480px',
+      // => @media (min-width: 480px) { ... }
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       backgroundImage: {
         'custom-header': 'linear-gradient(95.17deg, rgba(32, 245, 115, 0.16) 14.04%, rgba(32, 245, 115, 0.07) 73.87%)',
         //Define custom gradient
       },
       colors: {
-        'custom-green': 'rgba(32, 245, 115, 1)',  // Define the custom color
+        'custom-green': 'rgba(32, 245, 115, 1)',  // Define the custom green color
       },
       // adding custom google font manrope 
       fontFamily: {
